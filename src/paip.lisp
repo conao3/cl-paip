a7d6ee6c220ad7c56b7a96551b07363ffe4059bb
Naoya Yamashita <conao3@gmail.com>
2024-12-22 19:48:54 +0900

    pat-match constant ver

--- src/paip.lisp
+++ src/paip.lisp
@@ -1,13 +1,21 @@
 (defpackage #:paip/src/paip
   (:nicknames #:paip)
   (:use #:cl)
-  (:export #:main #:fib))
+  (:export #:main #:fib #:pat-match))
 (in-package #:paip)
 
 (defun fib (n)
   (if (<= n 2)
     1
     (+ (fib (- n 2)) (fib (- n 1)))))
+
+(defun hello ()
+  (print "asdf"))
+
+(defvar no-bindings '((t . t)))
+
+(defun pat-match (pattern input &optional (bindings no-bindings))
+  '((?Y . 4) (?X . 3)))
 
 (defun main ()
   (let ((args uiop:*command-line-arguments*))
--- test/paip.lisp
+++ test/paip.lisp
@@ -7,3 +7,7 @@
 
 (test fib-test
   (is (fib 10) 55))
+
+(test pat-match1
+      (is (pat-match '(?x (?or < = >) ?y) '(3 < 4))
+          '((?Y . 4) (?X . 3))))
